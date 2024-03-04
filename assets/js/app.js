8fd23669361bd9a13aa7bdccf8bcf60343a048ad
abdurDaily <abdurdaily@gmail.com>
2024-03-04 19:50:43 +0600

    hide test

---
+++ assets/js/app.js
@@ -0,0 +1,14 @@
+
+$(document).ready(function() {
+    
+    $('.slides').slick({
+        infinite: true,
+        slidesToShow: 1,
+        nextArrow: '.icon-right',
+        prevArrow: '.icon-left',
+        speed:500,
+        dots:true,
+        arrows: true,
+        dotsClass: 'container dots',
+    });
+});
